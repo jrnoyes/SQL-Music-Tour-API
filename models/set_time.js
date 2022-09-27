1b6e02f804c3775e212d9aa29621a5430b7ba5ee
Jason Noyes <jrnoyes1@gmail.com>
2022-09-26 20:46:26 -0500

    finish day 4 activities

---
+++ models/set_time.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class set_time extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  set_time.init({
+      event_id: DataTypes.INTEGER,
+      stage_id: DataTypes.INTEGER,
+      band) id: DataTypes.INTEGER, start_time: DataTypes.DATE, end_time: DataTypes.DATE, set_time_id: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'set_time',
+  });
+return set_time;
+};
